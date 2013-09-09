3120009ea1b4bb58a4c9d2d54d55b35423e6decb
Peter Monus <pete.monus@gmail.com>
2013-09-09 21:52:52 +1000

    Removed android app code. Added new asp.net site.
    New app site contains empty classes to be filled with required code.

---
+++ Source/PFWorkTimesheet/PFWorkTimesheet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PFWorkTimesheet.Global" Language="C#" %>
---
+++ Source/PFWorkTimesheet/TimesheetBusiness/TimesheetObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimesheetBusiness
+{
+    class TimesheetObject
+    {
+
+
+        public void TimesheetObject()
+        {
+        }
+
+        public string jobsite
+        {
+            get{ return jobsite; }
+            set { jobsite = value; }
+        }
+
+        public string employee
+        {
+            get { return employee; }
+            set { employee = value; }
+        }
+
+        public string date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public string code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        public string comments
+        {
+            get { return comments; }
+            set { comments = value; }
+        }
+
+        public string hoursWednesday
+        {
+            get { return hoursWednesday; }
+            set { hoursWednesday = value; }
+        }
+
+
+    }
+}
